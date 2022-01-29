a4c4626a5044be54b15a2210c7f2f3cbc2fbb6d2
Pietro Mazzini <36486557+myasn1k@users.noreply.github.com>
2022-01-29 17:35:57 +0100

    Update =template=.java

--- templates/=template=.java
+++ templates/=template=.java
@@ -1,14 +1,30 @@
-/*
- * %FFILE%
- * Copyright (C) %YEAR% %USER% <%MAIL%>
- *
- * Distributed under terms of the %LICENSE% license.
- */
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class %FILE%
 {
 	public static void main(String[] args) {
-		%HERE% 
+            // Read file into list of strings
+            List<String> infile;
+	    try (Stream<String> lines = Files.lines(Paths.get(args[0]))) {
+		infile = lines.collect(Collectors.toList());
+	    } catch(IOException e) {
+                return;
+            }
+
+            // Read first line into list of integers
+            Scanner scanner = new Scanner(infile.get(0));
+            List<Integer> firstline = new ArrayList<Integer>();
+            while (scanner.hasNextInt()) {
+                firstline.add(scanner.nextInt());
+            }
+	    
+	    %HERE%
     	}
 }
-
