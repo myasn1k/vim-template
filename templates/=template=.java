915294c11a19d32ef2bacd69d25cc463f98a1bb9
Pietro Mazzini <36486557+myasn1k@users.noreply.github.com>
2022-01-30 18:24:58 +0100

    Update =template=.java

--- templates/=template=.java
+++ templates/=template=.java
@@ -9,22 +9,22 @@ import java.util.stream.Stream;
 
 public class %FILE%
 {
-	public static void main(String[] args) {
-            // Read file into list of strings
-            List<String> infile;
-	    try (Stream<String> lines = Files.lines(Paths.get(args[0]))) {
-		infile = lines.collect(Collectors.toList());
-	    } catch(IOException e) {
-                return;
-            }
+    public static void main(String[] args) {
+        // Read file into list of strings
+        List<String> infile;
+        try (Stream<String> lines = Files.lines(Paths.get(args[0]))) {
+	    infile = lines.collect(Collectors.toList());
+        } catch(IOException e) {
+	    return;
+        }
 
-            // Read first line into list of integers
-            Scanner scanner = new Scanner(infile.get(0));
-            List<Integer> firstline = new ArrayList<Integer>();
-            while (scanner.hasNextInt()) {
-                firstline.add(scanner.nextInt());
-            }
-	    
-	    %HERE%
-    	}
+        // Read first line into list of integers
+        Scanner scanner = new Scanner(infile.get(0));
+        List<Integer> firstline = new ArrayList<Integer>();
+        while (scanner.hasNextInt()) {
+	    firstline.add(scanner.nextInt());
+        }
+
+        %HERE%
+    }
 }
