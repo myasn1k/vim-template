62901af9a4e806cf6ba0e0724dac9329ad9ef9f5
myasnik <mazzini.pietro@protonmail.com>
2022-09-20 17:22:10 +0200

    Update pwntools template: add riscv and aarch64 support

--- templates/=template=sol.py
+++ templates/=template=sol.py
@@ -13,32 +13,49 @@ import sys
 # context.arch = 'X86'
 
 #########################################################
-GDB_FLAV="gef" # pwndbg - gef - vanilla
-BINARY=""   # path to the binary file
-ENV={}      # list of enviroment variables, useful if you need to use a given library
-GDB=""      # commands to execute if you debug using gdb
-HOST=""     # host for remote connection
-PORT=1337   # port for remote connection
+GDB_FLAV="gef"    # pwndbg - gef - vanilla
+BINARY=""         # path to the binary file
+ENV={}            # list of enviroment variables, useful if you need to use a given library
+GDB=""            # commands to execute if you debug using gdb
+HOST=""           # host for remote connection
+PORT=1337         # port for remote connection
+DBG_PORT='12345'  # debug port, for different architectures
 #########################################################
 
 if len(sys.argv) < 2:
-    print("args: bin|net|ida|gdb\n")
+    print("args: bin|net|gdb\n")
     sys.exit(1)
 
 if sys.argv[1] == "bin":
-    p = process(BINARY, env=ENV)
+    if context.arch == 'aarch64':
+        p = process(['qemu-aarch64', '-L', '/usr/aarch64-linux-gnu', BINARY])
+    elif context.arch == 'riscv':
+        p = process(['qemu-riscv64', '-L', '/usr/riscv64-linux-gnu', BINARY])
+    else:
+        p = process(BINARY, env=ENV)
 elif sys.argv[1] == "net":
     p = remote(HOST, PORT)
-elif sys.argv[1] == "ida":
-    # remote ida debugger can bu found at ~/.wine/drive_c/Program\ Files/IDA\ 7.5/dbgsrv/linux_server
-    p = process("./linux_server", env=ENV)
-    p.recvuntil(b"0.1...")
 elif sys.argv[1] == "gdb":
-    p = process(BINARY, env=ENV)
     if GDB_FLAV == "vanilla":
-        gdb.attach(p, gdbscript=GDB)
+        if context.arch == 'aarch64':
+            p = process(['qemu-aarch64', '-L', '/usr/aarch64-linux-gnu', '-g', DBG_PORT, BINARY])
+            q = process("tmux split -v aarch64-linux-gnu-gdb -ex 'file " + BINARY + " ' -ex 'target remote localhost:" + DBG_PORT + "' -ex '" + GDB + "'", shell=True)
+        elif context.arch == 'riscv':
+            p = process(['qemu-riscv64', '-L', '/usr/riscv64-linux-gnu', '-g', DBG_PORT, BINARY])
+            q = process("tmux split -v riscv64-linux-gnu-gdb -ex 'file " + BINARY + " ' -ex 'target remote localhost:" + DBG_PORT + "' -ex '" + GDB + "'", shell=True)
+        else:
+            p = process(BINARY, env=ENV)
+            gdb.attach(p, gdbscript=GDB)
     else:
-        gdb.attach(p, gdb_args=["-ex", "init-" + GDB_FLAV], gdbscript=GDB)
+        if context.arch == 'aarch64':
+            p = process(['qemu-aarch64', '-L', '/usr/aarch64-linux-gnu', '-g', DBG_PORT, BINARY])
+            q = process("tmux split -v aarch64-linux-gnu-gdb -ex 'file " + BINARY + " ' -ex 'target remote localhost:" + DBG_PORT + "' -ex init-" + GDB_FLAV + " -ex '" + GDB + "'", shell=True)
+        elif context.arch == 'riscv':
+            p = process(['qemu-riscv64', '-L', '/usr/riscv64-linux-gnu', '-g', DBG_PORT, BINARY])
+            q = process("tmux split -v riscv64-linux-gnu-gdb -ex 'file " + BINARY + " ' -ex 'target remote localhost:" + DBG_PORT + "' -ex init-" + GDB_FLAV + " -ex '" + GDB + "'", shell=True)
+        else:
+            p = process(BINARY, env=ENV)
+            gdb.attach(p, gdb_args=["-ex", "init-" + GDB_FLAV], gdbscript=GDB)
 else:
     print("args: bin|net|ida|gdb")
     sys.exit(1)
